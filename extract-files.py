07e6d3c218a33efe717081c3c64a29b67095172f
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-08 16:06:45 +0300

    caza: allow sched_get_priority_max syscall for qsap_location
    Change-Id: Ieec938cf4c51caa65c2c246939a39fc9aa23b13a

--- extract-files.py
+++ extract-files.py
@@ -162,7 +162,8 @@ blob_fixups: blob_fixups_user_type = {
         'android.hardware.graphics.allocator-V2-ndk.so',
     ),
     'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
-        .add_line_if_missing('sched_get_priority_min: 1'),
+        .add_line_if_missing('sched_get_priority_min: 1')
+        .add_line_if_missing('sched_get_priority_max: 1'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
