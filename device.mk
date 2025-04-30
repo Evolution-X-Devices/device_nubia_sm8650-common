34396a706d529595107cbfd877b0c7b074ad64ab
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    caza: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- device.mk
+++ device.mk
@@ -103,6 +103,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
 
+# Camera
+$(call soong_config_set,camera,override_format_from_reserved,true)
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
