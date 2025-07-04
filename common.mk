4ef9a187e26df37a12fd5e2adf91a2ade991a9b5
hiroshi <hiroshi@tanuki.page>
2025-07-04 12:17:31 -0400

    sm8650-common: set TARGET_HAS_UDFPS Required to show UDFPS customization settings

--- common.mk
+++ common.mk
@@ -134,6 +134,8 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//$(COMMON_PATH):libudfps_extension.sm8650)
 
+TARGET_HAS_UDFPS := true
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
